aa19f3898aa173e3d63c9b7236f8c5ed52faad27
Hamza Awan <hamza_awan1996@hotmail.com>
2018-07-24 13:34:38 +0500

    Listed Candidates
    Added two candidates for testing and added test file to test your app before it is deployed to save ether. Also, set up a metamask and configured the client side based application.

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,26 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.24;
 
 contract Election {
-	string public candidate;
-	
-	function Election () public {
 
-		candidate = "Candidate 1";
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
 	}
+	
+	mapping (uint => Candidate) public candidates;
+	uint public candidatesCount;
+	
+
+	constructor () public {
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate (string _name) private {
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+	
 	
 }
